f34ce7301ff7ebd45c12dc629b29ba5c0218a7c7
chairina <chairina1109@gmail.com>
2025-04-17 11:16:17 +0700

    feat: fix responsiveness in homelayout, generallayout, investprofile

--- src/pages/investprofilepage.jsx
+++ src/pages/investprofilepage.jsx
@@ -29,6 +29,16 @@ export default function InvestProfilePage() {
         <div className='flex flex-col items-end w-full md:flex-row gap-10'>
           {/* Left Box */}
           <div className='flex-1'>
+            {/* Text content - stays on top */}
+            <div className='h-full w-full pt-20 md:pt-24 relative z-10 mb-4'>
+              <h1 className='text-white font-medium text-4xl md:text-3xl leading-tight tracking-normal font-inter'>
+                Ini dia hasil rekomendasi
+              </h1>
+
+              <h1 className='font-extrabold text-4xl md:text-4xl leading-tight tracking-normal font-inter text-[#FFD573] pl-0 md:pl-28'>
+                Investasi Syariah<span className='text-white'>mu!</span>
+              </h1>
+            </div>
             <div className='bg-white rounded-xl p-6 shadow'>
               <div className='flex items-start justify-between mb-4 gap-2'>
                 <div className='flex items-center gap-4'>
